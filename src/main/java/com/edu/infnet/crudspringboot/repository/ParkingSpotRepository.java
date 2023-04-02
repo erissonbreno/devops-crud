7db45457d818eb7a6f558190413b6fa64e25e9c6
erisson.batista@ask.com <erissonbreno@hotmail.com>
2023-04-02 14:20:46 -0300

    Added CRUD SpringBoot application

---
+++ src/main/java/com/edu/infnet/crudspringboot/repository/ParkingSpotRepository.java
@@ -0,0 +1,15 @@
+package com.edu.infnet.crudspringboot.repository;
+
+import com.edu.infnet.crudspringboot.model.ParkingSpotModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ParkingSpotRepository extends JpaRepository<ParkingSpotModel, UUID> {
+
+    boolean existsByLicensePlateCar(String licensePlateCar);
+    boolean existsByParkingSpotNumber(String parkingSpotNumber);
+    boolean existsByApartmentAndBlock(String apartment, String block);
+}
